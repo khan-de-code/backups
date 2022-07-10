d232299887769277d561a48494488142b3afb1c8
Dawson Batz <50726238+khan-de-code@users.noreply.github.com>
2022-07-09 23:33:03 -0600

    Update git_cleanup.sh

--- scripts/git_cleanup.sh
+++ scripts/git_cleanup.sh
@@ -1,6 +1,11 @@
 TEMP_VAR=`git rev-parse --abbrev-ref HEAD`;
 
 git checkout main;
+
 git pull;
+git fetch --tags;
+git fetch --prune;
+
 git checkout $TEMP_VAR;
+
 git branch --merged main | egrep -v -e '^\s\smain$' -e '^\* main$' | xargs -n 1 -r git branch -d;
